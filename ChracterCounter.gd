346841b7d1be3d4cca053642cd0ad22511b3f0a0
raymondem42 <stupidmake1@gmail.com>
2024-08-18 11:01:59 -0400

    Fixed the switch and the game logic, reading files too, it's all perfecto, now we just have to integrate with the H, and we should be alright I think

--- ChracterCounter.gd
+++ ChracterCounter.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var AnimalCheckFile = "res://animals.txt"
 @onready var ElementCheckFile = "res://elements.txt"
 @onready var FoodCheckFile = "res://food.txt"
-@onready var CountryCheckFile = "res://countires.txt"
+@onready var CountryCheckFile = "res://countries.txt"
 @onready var prompt = "res://prompts.txt"
 
 
@@ -21,6 +21,7 @@ var player1scores = []
 var player2scores = []
 var promptsInfo = []
 var random_index = randi() % 55
+
 
 func _ready() -> void:
 	timer.wait_time = 15.0
@@ -37,23 +38,28 @@ func _ready() -> void:
 	pick_random_prompt()
 	
 func pick_random_prompt():
-	if promptsInfo.size() > 0:
-
-		var selected_prompt = promptsInfo[random_index]
-		print(random_index)
-		print("Selected Prompt: ", selected_prompt)
-		
+	print(random_index)
+	if(random_index < 22):
+		if promptsInfo.size() > 0:
+				var selected_prompt = promptsInfo[random_index -1]
+				print(random_index)
+				print("Selected Prompt: ", selected_prompt)
+		else:
+			print("Selected Prompt: Any word, gogogo")
 
 
 #seeing if it's a word in general
 func check_string_in_file(input_string: String, file_path: String) -> bool:
-	var file = FileAccess.open((WordCheckFile), FileAccess.READ)
-	var file_content = file.get_as_text()
-	file.close()
-	if input_string in file_content:
-		return true
-	else:
-		print("word does not exist")
+	input_string = input_string.strip_edges().to_lower()  # Normalize the input	
+	var file = FileAccess.open((file_path), FileAccess.READ)
+	while not file.eof_reached():
+		var line = file.get_line().strip_edges().to_lower()
+		#print(line)
+		# Check if the current line matches the input string
+		if line == input_string:
+			file.close()
+			return true
+	
 	return false
 
 
@@ -134,118 +140,170 @@ func prompt_function(new_text):
 		1:
 			if "e" in new_text.to_lower():
 				print("that has an E")
+				return false
+			else:
+				print("no E here")
+				return true
 		2:
-			print("we at 2")
 			if "a" in new_text.to_lower():
 				print("that has an A")
+				return false
+			else:
+				print("no A here")
+				return true
 		3:
 			if new_text.to_lower().begins_with("y"):
 				print("begins wtih Y")
+				return true
 			else:
 				print("no y here")
+				return false
 		4:
 			if has_two_of_same_letter(new_text):
-				print("nah there's 2 letters here")
+				print("There's 2 letters here")
+				return true
+			else:
+				print("no 2 leter")
+				return false
 		5:
 			if is_palindrome(new_text):
 				print("yup that's a palindrome")
+				return true
 			else:
 				print("not a palindrome")
+				return false
 		6:
 			if contains_vowels(new_text):
 				print("yup that's 3 vowels")
+				return true
 			else:
 				print("not at least 3")
+				return false
 		7:
 			if new_text.to_lower().ends_with("ing"):
 				print("yup that's end with ing")
+				return true
 			else:
 				print("no it does not")
+				return false
 		8:
 			if contains_2_vowels(new_text):
 				print("yup that has 2")
+				return true
 			else:
 				print("no it does not")
+				return false
 		9:
 			if "j" in new_text.to_lower():
 				print("yup that has a J")
+				return true
 			else:
 				print("no it does not")
+				return false
 		10:
 			if new_text.to_lower().begins_with("ch"):
 				print("yup that starts with ch")
+				return true
 			else:
 				print("no it does not")
+				return false
 		11:
 			if "th" in new_text.to_lower():
 				print("yup that has a th")
+				return true
 			else:
 				print("no it does not")
+				return false
 		12:
 			if contains_no_vowels(new_text):
 				print("yup that has none")
+				return true
 			else:
 				print("no it does not")
+				return false
 		13:
 			if new_text.to_lower().ends_with("x"):
 				print("yup that ends in x")
+				return true
 			else:
 				print("no it does not")
+				return false
 		14:
 			if starts_and_ends_with_same_letter(new_text):
 				print("yup that starts and ends the same")
+				return true
 			else:
 				print("no it does not")
+				return false
 		15:
 			#seeing if Y is in the middle of the word
 			if contains_y_not_at_beginning_or_end(new_text):
 				print("yup that has y in the middle ")
+				return true
 			else:
 				print("no it does not")
+				return false
 		16:
 			#seeing if Y is in the middle of the word
 			if check_string_in_file(new_text, CountryCheckFile):
 				print("yup that's a country")
+				return true
 			else:
-				print("no it does not")
+				print("no it is does not")
+				return false
 		17:
 			#seeing if Y is in the middle of the word
 			if check_string_in_file(new_text, AnimalCheckFile):
 				print("yup that's a animal")
+				return true
 			else:
 				print("no it does not")
+				return false
 		18:
 			#seeing if Y is in the middle of the word
 			if check_string_in_file(new_text, ElementCheckFile):
 				print("yup that's a element")
+				return true
 			else:
 				print("no it does not")
+				return false
 		19:
 			#seeing if Y is in the middle of the word
 			if check_string_in_file(new_text, FoodCheckFile):
 				print("yup that's a food")
+				return true
 			else:
 				print("no it does not")
+				return false
 		20:
 			#seeing if Y is in the middle of the word
 			if check_string_in_file(new_text, AnimalCheckFile) and new_text.to_lower().begins_with("e"):
 				print("yup that's an animal that starts with e")
+				return true
 			else:
 				print("no it does not")
+				return false
 		21:
 			#seeing if Y is in the middle of the word
 			if check_string_in_file(new_text, AnimalCheckFile) and new_text.to_lower().begins_with("o"):
 				print("yup that's an animal that starts with e")
+				return true
 			else:
 				print("no it does not")
+				return false
 	if(random_index > 21):
-		check_string_in_file(new_text, WordCheckFile)
+		if(check_string_in_file(new_text, WordCheckFile)):
+			print("yup that's a real word")
+			return true
+		else:
+			return false
 
 
 func _on_text_submitted(new_text: String) -> void:		
 	if prompt_function(new_text):
 		timer.stop()
 		var char_count = new_text.length()
+		print(char_count)
 		if is_player1_turn:
 			player1Score += char_count
 			result_label.text = "Player 1 Score: " + str(player1Score)
